2b2a0c6843fb37bc26ffdc6af9dcc0505043ea08
bidkar <bidkar.aragon@udo.mx>
2018-08-30 11:23:20 -0600

    Clase Saludo
    + se creó la clase Saludo con una propiedad Mensaje

---
+++ DemoGitFromVS/Saludo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoGitFromVS
+{
+    class Saludo
+    {
+        public string Mensaje { get; set; }
+
+        public Saludo()
+        {
+            Mensaje = "Hola desde Visual Studio";
+        }
+    }
+}
